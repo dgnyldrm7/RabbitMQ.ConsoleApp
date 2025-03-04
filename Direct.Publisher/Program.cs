c72ddf5df8d0c5b695a6d0ce9f37eddde053cabe
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-03-04 13:29:16 +0300

    Direct Exchange Type
    direct exchange type

---
+++ Direct.Publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿// Create a connection factory
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://zfyeenuc:94kom0sr9-KZ3C6pVqDxu3TSTH2ysU9s@woodpecker.rmq.cloudamqp.com/zfyeenuc");
+
+// Create a connection
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+string exhangeName = "direct-Exchange-test";
+string routingKeyDirect = "direct-routing-key";
+
+
+//Direct Exchange Declare
+await channel.ExchangeDeclareAsync(exchange: exhangeName, ExchangeType.Direct, true, false);
+
+// Create a message with the console app
+Console.WriteLine("Enter your message: ");
+string? message = Console.ReadLine();
+
+if (string.IsNullOrEmpty(message))
+{
+    message = string.Empty;
+}
+
+// Convert the message to a byte array
+byte[] bodyMessage = Encoding.UTF8.GetBytes(message);
+
+// Publish the message to the queue
+await channel.BasicPublishAsync
+    (
+        exchange: exhangeName,
+        routingKey: routingKeyDirect,
+        body: bodyMessage
+    );
+
+
+string bodyMessageToString = Encoding.UTF8.GetString(bodyMessage);
+
+Console.WriteLine("Message sent successfully!");
+Console.WriteLine($"Your message is " + bodyMessageToString);
+Console.Read();
---
+++ Direct.Subscriber/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.Uri = new Uri("amqps://zfyeenuc:94kom0sr9-KZ3C6pVqDxu3TSTH2ysU9s@woodpecker.rmq.cloudamqp.com/zfyeenuc");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+//Öncelikle bir exchange tanımlanır/oluşturulur.
+await channel.ExchangeDeclareAsync(exchange: "direct-Exchange-test", ExchangeType.Direct, true, false, null);
+
+//Öncelikle bir queue tanımlanır/oluşturulur.
+string randomQueue = await channel.QueueDeclareAsync();
+
+string? route = Console.ReadLine();
+
+if(string.IsNullOrEmpty(route))
+{
+    route = "direct-routing-key";
+}
+
+//Oluşturulan queue exchange ile bind edilir.
+await channel.QueueBindAsync
+    (
+        queue: randomQueue, 
+        exchange: "direct-Exchange-test", 
+        routingKey: route
+    );
+
+// Create a consumer
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+// Start consuming
+await channel.BasicConsumeAsync(queue: randomQueue, autoAck: true, consumer: consumer);
+
+
+
+// Consume the message
+consumer.ReceivedAsync += async (ch, ea) =>
+{
+    string content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+    Console.WriteLine($"Message received: {content}");
+
+    await Task.Delay(200);
+
+    Console.WriteLine("Message processed!");
+};
+
+
+
+Console.WriteLine("Consumer started!");
+Console.ReadLine();
